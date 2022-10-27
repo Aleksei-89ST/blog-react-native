6ef25831871b12ebcfa1529b90f8a628d7e96f8a
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-10-27 22:32:13 +0400

    first post

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Post = styled.View`
+flex-direction: row;
+padding:15px;
+border-bottom-width: 1px;
+border-bottom-color: rgba(0,0,0,0.1);
+border-bottom-style: solid;
+margin-top: 10px;
+`;
+
+const PostDetails = styled.View`;
+
+`
+
+const PostDate = styled.Text`
+font-size:12px;
+color: rgba(0,0,0,0.4);
+margin-top: 2px;
+`;
+
+
+const PostTitle = styled.Text`
+font-size: 16px;
+font-weight: 700;
+`;
+
+const PostImage = styled.Image`
+width: 100px;
+height: 100px;
+border-radius:12px;
+margin-right:12px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Post>
+        <PostImage source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrvMUm5Y7_aJDKxv8_fQTbhMHaYOlevDfbzg&usqp=CAU"}}/>
+        <PostDetails>
+        <PostTitle>Тестовая статья</PostTitle>
+        <PostDate>27/10/2022</PostDate>
+        </PostDetails>
+      </Post>
+      <StatusBar theme="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
